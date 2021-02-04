6dabfbca56df3315ab651090c26cf4958cc3bc7f
test <test@test.com>
2021-02-04 11:02:30 +0100

    dist + zmiana w package.json

---
+++ dist/js/chunk-2d0d63f1.91610006.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d63f1"],{7277:function(a,r,t){"use strict";t.r(r);var e=function(){var a=this,r=a.$createElement,t=a._self._c||r;return t("v-container",{attrs:{fluid:"",tag:"section"}},[t("v-navigation-drawer",{attrs:{dark:"",app:"",width:"260"},model:{value:a.drawer,callback:function(r){a.drawer=r},expression:"drawer"}},[t("v-img",{attrs:{src:""}})],1)],1)},n=[],c={},i=c,s=t("2877"),l=Object(s["a"])(i,e,n,!1,null,null,null);r["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0d63f1.91610006.js.map
